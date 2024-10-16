d6d4323392e7ce7638f6e999c23cd2f6797ba74c
angiegz2 <angie02yarce@gmail.com>
2024-10-16 00:49:34 -0500

    Mostrar página de consulta

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/global.css';
 import AirplaneForm from './components/RegisterAirplane';
+import ConsultAirplane from './components/ConsultAirplane';
 
 const app = () => {
   return (
     <div>
       <AirplaneForm />
+      <ConsultAirplane />
     </div>
   );
 };
